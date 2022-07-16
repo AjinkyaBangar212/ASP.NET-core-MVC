044b3a069c0589f3569a25d59e2bdbe29274abd0
Ajinkya Bangar <ajinkyabangar212@gmail.com>
2022-07-16 10:17:01 +0530

    Add project files.

---
+++ ASP.NET core MVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using ASP.NET_core_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_core_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult AboutUs()  // adding new custom action
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()  // adding new custom action
+        {
+            return View();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
